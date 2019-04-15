53725b4279e802feb142f60f47e2f47bd8f883ab
Shreyas Lad <shadowtemplates@gmail.com>
2019-04-15 00:20:43 +0000

    fixed prompt a bit

--- main.sh
+++ main.sh
@@ -1,17 +1,18 @@
 main () {
-    printf "> "
+    printf ">>> "
     read input
     echo "You said $input"
     main
 }
 
-echo "Welcome to Smalltalk. Type in the console below to converse with the chatbot. If you need help, type in help."
+echo " "
+echo "Welcome to Smalltalk. Type in the console below to converse with the chatbot. If you need help, type in help()."
 
-printf "> "
+printf ">>> "
 read input
 if [[ -z "$input" ]];
 then
-    printf "please enter input"
+    echo "You may not leave these fields blank. If you are unsure of what to do, type help()"
     main
 else
     echo "You said $input"
