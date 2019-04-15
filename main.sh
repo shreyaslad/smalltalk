25ebfa41fdd31a72ce73407ca63deb736ec44e89
Shreyas Lad <shadowtemplates@gmail.com>
2019-04-14 19:07:09 -0700

    created curl request, will do something with jq later

--- main.sh
+++ main.sh
@@ -20,6 +20,7 @@ main () {
             keep
         fi
     fi
+    curl -H "Content-Type: application/json; charset=utf-8"  -H "Authorization: Bearer ya29.c.ElrsBk3hmgNEa7qrBndn9fpD0s5hpCwN1qrRwzEfEZhaz8BzyRh2YGvPA4fCoAJvu_5MkJmq2kefQpjdqc853cNe2lTMgbanwbu9IWT1Sg2HH53R36juasSGWdo"  -d "{\"queryInput\":{\"text\":{\"text\":\"$input\",\"languageCode\":\"en\"}},\"queryParams\":{\"timeZone\":\"America/Los_Angeles\"}}" "https://dialogflow.googleapis.com/v2/projects/smalltalk-bash/agent/sessions/2613a9b3-0762-ea0b-746e-50127441b643:detectIntent"
     main
 }
 
