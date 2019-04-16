990c85547b0043bbf1647cd733aa0ad5e30a3153
Penetratingshot <shadowtemplates@gmail.com>
2019-04-15 19:24:49 -0700

    makes prediction from predefined message and outputs it to console and file

--- chatbot/main.py
+++ chatbot/main.py
@@ -12,3 +12,13 @@ engine = SnipsNLUEngine(config=CONFIG_EN)
 engine.fit(dataset)
 parsing = engine.parse(u"Hey, lights on in the lounge !")
 print(json.dumps(parsing, indent=2))'''
+
+with io.open("./data/dataset.json") as f:
+    dataset = json.load(f)
+
+engine.fit(dataset)
+parsing = engine.parse(u"What's the weather for sf?")
+print(json.dumps(parsing, indent=2))
+
+file = open("./data/output.json", "w")
+file.write(json.dumps(parsing, indent=2))
