4f95ca065830f3cbb62ccf1f8b3469083c54905e
Penetratingshot <shadowtemplates@gmail.com>
2019-04-17 20:06:03 -0700

    added write to python script

--- main.py
+++ main.py
@@ -4,9 +4,16 @@ from snips_nlu import SnipsNLUEngine
 
 nlu_engine = SnipsNLUEngine()
 
+with open("./data/input.json") as input_file:
+    input_dict = json.load(input_file)
+
+input = input_dict['input']
+
 with io.open("./chatbot/data/dataset.json") as f:
     sample_dataset = json.load(f)
 
 nlu_engine.fit(sample_dataset)
-parsing = nlu_engine.parse(u"weather for dublin california")
-print(json.dumps(parsing, indent=2))
+parsing = nlu_engine.parse(u"%s" % input)
+
+file = open("./chatbot/data/output.json", "w")
+file.write(json.dumps(parsing, indent=2))
